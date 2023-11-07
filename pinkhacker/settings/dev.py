0990971c018bf771cfb3ceeabf2798f00b8c1b05
kraboobee <kraboobee@gmail.com>
2023-11-07 11:17:30 +0200

    Create project, start blog app

---
+++ pinkhacker/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-e69-1q#3sxrvx(qw^y7k=$(x+47lo=o7^cyoe56-v(u*ftl%4s"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
